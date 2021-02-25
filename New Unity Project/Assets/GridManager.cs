49216543270f3b861371c6f81913757b0a715057
Andres <anfurtivo1@hotmail.com>
2021-02-26 00:06:42 +0100

    Grid mapa
    Se ha creado el grid del mapa

---
+++ New Unity Project/Assets/GridManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public Sprite sprite;
+    public GameObject objeto;
+    public int columnas=3;
+    public int filas=3;
+    int[,] grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       grid = new int[columnas, filas];
+        for (int i = 0; i < columnas; i++)
+        {
+            for (int j = 0; j < filas; j++)
+            {
+                grid[i, j] = Random.Range(0, 10);
+                spawnTile(i, j);
+            }
+        }
+}
+
+    private void spawnTile(int x, int y)
+    {
+        Instantiate(objeto, new Vector3(x, y, 0), Quaternion.identity);
+        objeto.transform.position = new Vector3(x,y);
+    }
+}
