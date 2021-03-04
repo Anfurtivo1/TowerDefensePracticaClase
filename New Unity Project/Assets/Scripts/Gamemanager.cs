eebf9d5b43adaf05221aed68cf36068679d517bb
Andres <anfurtivo1@hotmail.com>
2021-03-04 01:00:19 +0100

    Mapa Actualizado y Creación Pokemon En Mapa
    Se ha cambiado el estilo del mapa por uno mas simple que consiste en varios rectángulos con un collision y se creará en el centro, de momento, solo un Flareon. Se esta experimentando con la generación de enemigos y disparar.

---
+++ New Unity Project/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public GameObject objeto;
+    public int dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Cast a ray straight down.
+            RaycastHit2D hit = Physics2D.Raycast(rayo.origin,rayo.direction);
+
+            // If it hits something...
+                if (hit.collider != null && hit.collider.tag.Equals("Cuadricula"))
+                {
+                //Para que se ponga en el centro de la cuadricula
+                    Transform cuadricula = hit.collider.transform;
+                    if (cuadricula.childCount==0)
+                    {
+                        Debug.Log("Se ha pulsado el boton izquierdo");
+                        GameObject pokemon=Instantiate(objeto, hit.collider.transform.position, objeto.transform.rotation);
+                        dinero = dinero - 100;
+                        pokemon.transform.SetParent(cuadricula);
+                    }
+                
+                }
+            }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Proyectil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour
+{
+    public int velocidad;
+    public int ataque;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
