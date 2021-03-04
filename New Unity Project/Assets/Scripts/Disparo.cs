c1bb986cbc2f7c9bfc3076df838469bacf1e125f
Andres <anfurtivo1@hotimail.com>
2021-03-04 15:30:48 +0100

    Movimiento proyectiles
    Se ha conseguido que Flareon genere proyectiles y se muevan pero no aparecen justo donde se crea. Ademas se ha corregido el color de los sprites y el borde negro que tenían.

---
+++ New Unity Project/Assets/Scripts/Disparo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public GameObject proyectil;
+    public Transform boca;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            GameObject p = Instantiate(proyectil, boca.position, boca.rotation);
+            Destroy(p, 2);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- New Unity Project/Assets/Scripts/Proyectil.cs
+++ New Unity Project/Assets/Scripts/Proyectil.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Proyectil : MonoBehaviour
 {
-    public int velocidad;
-    public int ataque;
+    public int velocidad = 3;
+    public int ataque = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,20 @@ public class Proyectil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.right * velocidad * Time.deltaTime;
     }
+
+
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Enemigo")
+    //    {
+    //        //Si el ataque colisiona contra un objeto con el tag 'Enemigo', se decrementan las vidas de dicho enemigo
+    //        other.gameObject.GetComponent<ComportamientoEnemigo>().Vidas--;
+
+    //        //Destruimos el objeto cuando colisione contra un enemigo
+    //        Destroy(gameObject);
+    //    }
+    //}
 }
