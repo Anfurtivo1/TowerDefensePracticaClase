189b271beaa88ad8912c9107bd51bd43fbac1546
Jose María Gestal <jmgestal00>
2021-03-10 01:49:28 +0100

    Introduccion de los sprites de los enemigos y la secuencia del 1er nivel
    Se introducen los sprites de los enemigos y que ahora puedan spawnear en todas las filas. Tambien se introduce la secuencia de enemigo que tendrá el primer nivel

--- New Unity Project/Assets/Scripts/GeneradorEnemigos.cs
+++ New Unity Project/Assets/Scripts/GeneradorEnemigos.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class GeneradorEnemigos : MonoBehaviour
 {
-    public GameObject Enemigo;
-    public Transform portal;
+    public List <GameObject> Enemigo;
+    public List <Transform> portal;
     private GameObject go;
+    private int[] listaenemigos = new int[] { 0, 0, 0, 0, 1, 0, 0, 2, 1, 0, 0, 1, 0, 2, 2, 0, 1, 1, 2, 1 };
+    private int[] listaportales = new int[] { 0, 2, 3, 1, 2, 4, 4, 0, 2, 3, 1, 1, 0, 0, 3, 4, 3, 4, 2, 0 };
+    private int i = 0;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
         while (true)
         {
-            yield return new WaitForSeconds(3);
-            go = Instantiate(Enemigo, portal.position, portal.rotation) as GameObject;
+            yield return new WaitForSeconds(1);
+            go = Instantiate(Enemigo[listaenemigos[i]], portal[listaportales[i]].position, portal[listaportales[i]].rotation) as GameObject;
+            if (i<20)
+            {
+                i++;
+            }
         }
 
     }
